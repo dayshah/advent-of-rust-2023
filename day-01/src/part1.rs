bc04feb40e637306b6e6c72217bb9e6882a0ca11
dayshah <dhyeyhshah@gmail.com>
2023-12-31 21:48:16 -0500

    fix day1

--- day-01/src/part1.rs
+++ day-01/src/part1.rs
@@ -1,7 +1,7 @@
 
-pub fn first_star(words: &Vec<String>) -> u32 {
+pub fn first_star(words: &Vec<String>) -> Option<u32> {
     words.iter()
-        .filter_map(|word| -> Option<u32> {
+        .map(|word| -> Option<u32> {
             (
                 word.chars()
                     .find(|c| c.is_numeric())?
